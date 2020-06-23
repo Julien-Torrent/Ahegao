e57ac783adf699a8792702ae4e2520b3f12e31de
Julien Torrent <julien.torrent@outlook.com>
2020-06-23 11:38:44 +0200

    More comments

--- Ahegao/Data/SiteContext.cs
+++ Ahegao/Data/SiteContext.cs
@@ -7,19 +7,29 @@ using System.Threading.Tasks;
 
 namespace Ahegao.Data
 {
+    /// <summary>
+    /// Class to access the Sites database
+    /// </summary>
     public class SiteContext : DbContext
     {
         public DbSet<Site> Sites { get; set; }
 
-        public SiteContext(DbContextOptions<SiteContext> options) : base(options)
-        { 
-        }
+        public SiteContext(DbContextOptions<SiteContext> options) : base(options) { }
 
+        /// <summary>
+        /// Get all the sites from the database asynchronously
+        /// </summary>
+        /// <returns>The list of retieved sites</returns>
         public async Task<List<Site>> GetAllAsync()
         {
             return await Sites.ToListAsync();
         }
 
+        /// <summary>
+        /// Convert the site Id to the ISite Type
+        /// </summary>
+        /// <param name="id">Id of the site</param>
+        /// <returns>The Type T to use in the HentaiParser<T></returns>
         public Type IdToType(int id)
         {
             return id switch
